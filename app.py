548ea89c4dc7f88d8caf6f5bdddeed1c6c8494c6
William <nettee.liu@gmail.com>
2018-03-12 22:22:51 +0800

    add flask-restful

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask.ext.restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+accounts = {}
+
+investmentProjects = {}
+
+class AccountList(Resource):
+
+    def get(self):
+        return accounts
+
+class InvestmentProjectList(Resource):
+
+    def get(self):
+        return investmentProjects
+
+api.add_resource(AccountList, '/accounts')
+api.add_resource(InvestmentProjectList, '/investment_projects')
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
