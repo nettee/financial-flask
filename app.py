6761380067f2d17e02d1cab9411671b5990e5aea
William <nettee.liu@gmail.com>
2018-03-16 17:11:59 +0800

    account list GET

--- app.py
+++ app.py
@@ -18,10 +18,29 @@ class CandidateAccount:
             'name': self.name,
         }
 
+class Account:
+
+    def __init__(self, type_, **kwargs):
+        self.type_ = type_
+        self.kwargs = kwargs
+
+    def as_dict(self):
+        d = {'type': self.type_}
+        d.update(self.kwargs)
+
+        if 'huabei' in self.kwargs:
+            huabei = self.kwargs['huabei']
+            d['huabei'] = {'type': huabei.type_}
+            d['huabei'].update(huabei.kwargs)
+
+        return d
+
 accounts = [
-    {'a': 1},
-    {'b': 2},
-    {'c': 3},
+    Account(type_='CASH', remark='钱包A', balance='976.00'),
+    Account(type_='CREDIT_CARD', remark='', bankCardNumber='4237', creditLimit='2000', billDate=1, paymentDate=10, arrears='1234'),
+    Account(type_='DEBIT_CARD', remark='', bankCardNumber='669395', balance='6134.77'),
+    Account(type_='ALIPAY', remark='', balance='16431.91', yuebao=None, 
+        huabei=Account(type_='HUABEI', creditLimit='2000', billDate=1, paymentDate=9, arrears='413.43')),
 ]
 
 candidate_accounts = [
@@ -40,7 +59,7 @@ investmentProjects = {}
 class AccountList(Resource):
 
     def get(self):
-        return accounts
+        return [a.as_dict() for a in accounts]
 
 class CandidateAccountList(Resource):
 
