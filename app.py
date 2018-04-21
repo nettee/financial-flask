c4e3f8c9628fa5701239ac71573474eb4990d20f
William <nettee.liu@gmail.com>
2018-04-21 16:21:30 +0800

    delete account

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import uuid
+
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, abort
 
 app = Flask(__name__)
 api = Api(app)
@@ -21,11 +23,12 @@ class CandidateAccount:
 class Account:
 
     def __init__(self, type_, **kwargs):
+        self.uuid = uuid.uuid1()
         self.type_ = type_
         self.kwargs = kwargs
 
     def as_dict(self):
-        d = {'type': self.type_}
+        d = {'uuid': str(self.uuid), 'type': self.type_}
         d.update(self.kwargs)
 
         if 'huabei' in self.kwargs:
@@ -35,13 +38,16 @@ class Account:
 
         return d
 
-accounts = [
+account_list = [
     Account(type_='CASH', remark='钱包A', balance='976.00'),
     Account(type_='CREDIT_CARD', remark='', bankCardNumber='4237', creditLimit='2000', billDate=1, paymentDate=10, arrears='1234'),
     Account(type_='DEBIT_CARD', remark='', bankCardNumber='669395', balance='6134.77'),
     Account(type_='ALIPAY', remark='', balance='16431.91', yuebao=None, 
         huabei=Account(type_='HUABEI', creditLimit='2000', billDate=1, paymentDate=9, arrears='413.43')),
 ]
+accounts = { str(a.uuid) : a for a in account_list }
+
+print(accounts)
 
 candidate_accounts = [
     CandidateAccount(type_='CASH', name='现金钱包'),
@@ -59,7 +65,7 @@ investmentProjects = {}
 class AccountList(Resource):
 
     def get(self):
-        return [a.as_dict() for a in accounts]
+        return [a.as_dict() for a in accounts.values()]
 
     def post(self):
         json_data = request.get_json()
@@ -67,8 +73,18 @@ class AccountList(Resource):
         type_ = json_data['type']
         del json_data['type']
         account = Account(type_, **json_data)
-        accounts.append(account)
+        accounts[account.uuid] = account
         return account.as_dict(), 201
+
+class Account(Resource):
+
+    def delete(self, id_):
+        if not id_ in accounts:
+            abort(404, message='Account {} does not exist'.format(id_))
+        
+        del accounts[id_]
+        # XXX workaround: if return 204, the client won't go
+        return '', 201
 
 class CandidateAccountList(Resource):
 
@@ -81,6 +97,7 @@ class InvestmentProjectList(Resource):
         return investmentProjects
 
 api.add_resource(AccountList, '/accounts')
+api.add_resource(Account, '/accounts/<id_>')
 api.add_resource(CandidateAccountList, '/candidate_accounts')
 api.add_resource(InvestmentProjectList, '/investment_projects')
 
